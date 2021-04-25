7e3b653f300134d60cf8da8bc2d94a87f1d600fd
Oren Levy <olevybusiness@gmail.com>
2021-04-25 10:23:06 +1000

    updated comments and fixed bugs

--- Assignment/Server/user.py
+++ Assignment/Server/user.py
@@ -51,38 +51,3 @@ class User:
     def incr_pass_atmpt(self):
         print(self.passAtt)
         self.passAtt += 1
-
-
-#
-# def authenticate_user_login(user, userData):
-#     username = user.get_username()
-#     password = user.get_password()
-#     # Check username exists
-#     if userData.get(username) is not None:
-#         # User is blocked
-#         if user.get_blocked() is True:
-#             # Change to unblocked if enough time has past
-#             if (time.time() - user.get_block_time()) > 20:
-#                 user.set_blocked(False)
-#             # Not enough time has past, user remains blocked.
-#             else:
-#                 return "USER_BLOCKED"
-#
-#         # Wrong password, increment failed attempts
-#         if userData[username]['password'] != password:
-#             user.incr_pass_atmpt()
-#             # Limit reached, block user, start block-timer, and restart count for passwordAttempts
-#             if user.get_pass_atmpt() == user.get_pass_atmpt_limit():
-#                 user.set_blocked(True)
-#                 user.start_block_timer(time.time())
-#                 user.restart_pass_atmpt()
-#                 return "USER_BLOCKED"
-#
-#             return "INVALID_PASSWORD"
-#
-#         # Passwords matches AND not blocked - approve login
-#         return "LOGIN_SUCCESS"
-#
-#     # Username doesn't exist
-#     else:
-#         return "INVALID_USERNAME"
